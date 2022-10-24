351a54c18711bb13f5dd66d7937d6c31123794ac
yooseungbum <103993616+yooseungbum@users.noreply.github.com>
2022-10-24 16:55:48 +0900

    Add files via upload
    crawling(lotto, powerBall)

--- crawling.py
+++ crawling.py
@@ -6,21 +6,21 @@ from bs4 import BeautifulSoup as bs
 
 
 def lotto_crawling():
-    minDrwNo = 1        #취득하고 싶은 회차 시작
-    maxDrwNo = 1037     #취득하고 싶은 회차 종료
-    drwtNo1 = []        #1등 첫번째 번호
-    drwtNo2 = []        #1등 두번째 번호
-    drwtNo3 = []        #1등 세번째 번호
-    drwtNo4 = []        #1등 네번째 번호
-    drwtNo5 = []        #1등 다섯번째 번호
-    drwtNo6 = []        #1등 여섯번째 번호
-    bnusNo = []         #보너스 번호
-    drwNoDate = []      #로또 추첨일
-    firstAccumamnt = [] #1등 전체상금
-    totSellamnt = []    #회차 전체상금
-    firstWinamnt = []   #1등 수령상금
-    firstPrzwnerCo = [] #1등 당첨인원
-    drwtNo = []         #1등 전체번호
+    minDrwNo = 1            #취득하고 싶은 회차 시작
+    maxDrwNo = 2            #취득하고 싶은 회차 종료  (최종 : 1038 회 (2022-10-23))
+    drwtNo1 = []            #1등 첫번째 번호
+    drwtNo2 = []            #1등 두번째 번호
+    drwtNo3 = []            #1등 세번째 번호
+    drwtNo4 = []            #1등 네번째 번호
+    drwtNo5 = []            #1등 다섯번째 번호
+    drwtNo6 = []            #1등 여섯번째 번호
+    bnusNo = []             #보너스 번호
+    drwNoDate = []          #로또 추첨일
+    firstAccumamnt = []     #1등 전체상금
+    totSellamnt = []        #회차 전체상금
+    firstWinamnt = []       #1등 수령상금
+    firstPrzwnerCo = []     #1등 당첨인원
+    drwtNo = []             #1등 전체번호
 
     # 지정한 시작 회차 부터 종료 회차 까지 취득
     for i in range(minDrwNo, maxDrwNo + 1):
@@ -52,13 +52,48 @@ def lotto_crawling():
 
 def powerBall_crawling():
 
-    page = "https://www.gglotto1.com/kr/lottery_result/powerball_result.asp"
-    html = urllib.request.urlopen(page)
-    soup = bs(html, "html.parser")
+    minPage = 1         # crawling 할 시작 페이지
+    maxPage = 1         # crawling 할 마지막 페이지 (2004.2.19 101페이지까지 존재)
+    drwDate = []        # 추첨일자
+    drwpNo1 = []        # 당첨 1번째 번호
+    drwpNo2 = []        # 당첨 1번째 번호
+    drwpNo3 = []        # 당첨 1번째 번호
+    drwpNo4 = []        # 당첨 1번째 번호
+    drwpNo5 = []        # 당첨 1번째 번호
+    drwpNo6 = []        # 당첨 1번째 번호
+    drwpNo = []         # 당첨 전체 번호
+    drwResult = []      # 당첨 결과
+    firstWinamnt = []   # 1등 당첨금
 
-    # elements = soup.select('#ajaxthislist > div.tablebox1 > div > table > tbody > tr')
+    for i in range(minPage, maxPage + 1):
 
-    elements = soup.select('div#tablebox2 > tbody > tr.txtcenter')
-    print(elements)
+        page = "https://www.lottousa.co.kr/result/power_result.php?page="+ str(i)
+        html = urllib.request.urlopen(page)
+        soup = bs(html, "html.parser")
+
+        # elements = soup.select('#ajaxthislist > div.tablebox1 > div > table > tbody > tr')
+
+        tr_list = soup.select('#power > div.content > table > tbody > tr')
+        for tr in tr_list:
+            td_list = tr.find_all('td')
+            for td, item in enumerate(td_list):
+                if td == 1:
+                    drwDate.append(item.text)
+                if td == 2:
+                    drwpNo1.append(item.text.split('\n')[2])
+                    drwpNo2.append(item.text.split('\n')[3])
+                    drwpNo3.append(item.text.split('\n')[4])
+                    drwpNo4.append(item.text.split('\n')[5])
+                    drwpNo5.append(item.text.split('\n')[6])
+                    drwpNo6.append(item.text.split('\n')[7])
+                    drwpNo.append(item.text.replace('\n', ' ').strip())
+                if td == 3:
+                    drwResult.append(item.text)
+                if td == 4:
+                    firstWinamnt.append(item.text.split(',')[0] + str(',000,000'))
+
+    print(drwpNo1)
+
+
 
 
--- main.py
+++ main.py
@@ -1,6 +1,46 @@
 from crawling import lotto_crawling, powerBall_crawling
+import sqlalchemy as db
+import yaml
+import pandas as pd
 
-# Press the green button in the gutter to run the script.
+# 서버접속정보 등 설정값 로딩.
+def load_config():
+    global config
+    with open("config.yaml", "r", encoding='utf8') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+# Toysmyth:3308 MariaDB에 접속
+def mysql_connect():
+    # Connect to a MySQL server
+    # :return connection: Global MySQL database connection
+
+    global connection
+    connection = db.create_engine(
+        "mysql+pymysql://"
+        + str(config["my_user"])
+        + ":"
+        + str(config["my_pw"])
+        + "@"
+        + str(config["my_addr"])
+        + ":"
+        + str(config["my_port"])
+        + "/"
+        + str(config["my_db"])
+    )
+
+# 쿼리를 실행하여 데이터를 df로 반환
+def run_query(sql):
+    # Runs a given SQL query via the global database connection.
+    # param sql: MySQL query
+    # return: Pandas dataframe containing results
+
+    return pd.read_sql(sql, connection)
+
+# DB 접속 종료
+def db_disconnect():
+    # Closes the MySQL database connection.
+    connection.dispose()
+
 if __name__ == '__main__':
     # lotto_crawling()
     powerBall_crawling()
